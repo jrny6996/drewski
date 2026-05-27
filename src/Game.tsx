12bff14d67fa2e0634b98183ca81a610c36e3e15
Jonathan Clark <thejonnyc9@gmail.com>
2026-05-27 13:11:59 -0400

    events

--- src/Game.tsx
+++ src/Game.tsx
@@ -4,8 +4,8 @@ import GameState from './engine'
 function Game() {
     const canvasRef = useRef<null | HTMLCanvasElement>(null)
     const [game, setGame] = useState<null | GameState>(null)
-   
-   
+
+
     useEffect(() => {
         if (canvasRef.current === null) return
         const canvas = canvasRef.current
@@ -22,16 +22,51 @@ function Game() {
     }, [])
 
 
-    useEffect(()=>{
-        if(!game) return
-        else{
-            setInterval(()=>{
+    useEffect(() => {
+        if (!game || !canvasRef.current
+        ) return
+        else {
+            setInterval(() => {
                 game.update()
-            }, Math.floor(1000/60))
+            }, Math.floor(1000 / 60))
 
+        }   
+        let initialPos = []
+        const pxToMatrixPos = (x:number, y:number)=>{
+            let i =0
+            let j = 0
+            return [i, j]
         }
+        const canvasEl = canvasRef.current!
 
-    },[game])
+        const rect = canvasEl.getBoundingClientRect()
+
+        const handleMove = (e: PointerEvent) => {
+            console.log("ptrMove", e?.clientX - rect.left
+                , e?.clientY - rect.top
+            )
+        }
+        const handleDown = (e: PointerEvent) => {
+            console.log("ptrDown", e?.clientX - rect.left
+                , e?.clientY - rect.top
+            )
+            canvasEl.addEventListener("pointermove", handleMove)
+        }
+        canvasEl.addEventListener("pointerdown", handleDown)
+
+        canvasEl.addEventListener("pointerup", () => {
+            canvasEl.removeEventListener("pointermove", handleMove)
+
+        })
+
+
+        return (() => {
+            canvasEl.removeEventListener("pointerdown", handleDown)
+            canvasEl.removeEventListener("pointermove", handleMove)
+        })
+
+
+    }, [game])
 
 
 
@@ -40,15 +75,7 @@ function Game() {
         <div>
 
             <canvas ref={canvasRef} style={{ background: "white" }} />
-            {/* {
-                game && (
-                    <div>
-                        <button onClick={() => {
-                            alert("hello")
-                        }}>exists</button>
-                    </div>
-                )
-            } */}
+
         </div>
     )
 }
